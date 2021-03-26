1be37d59d5c6a165098c1d890b8bf30ddda24f2c
Aliaksandr Snitko <pennywisexy@gmail.com>
2021-03-26 16:39:42 +0300

    finish resize table

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,22 +3,29 @@ const CODES = {
   Z: 90
 };
 
-function toCell(cell) {
+function toCell(_, col) {
   return `
-    <div class="cell">${cell}</div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
   `;
 }
 
 function createRow(idx, content) {
+  const resize = idx ? `<div class="row-resize" data-resize="row"></div>` : '';
   return `
-    <div class="row">
-      <div class="row-info">${idx ? idx : ''}</div>
+    <div class="row" data-type="resizable" data-resize="row">
+      <div class="row-info">
+        ${idx ? idx : ''}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `;
