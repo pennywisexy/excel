8517578eaad0694f1109babe07365d0de8d465a5
Aliaksandr Snitko <pennywisexy@gmail.com>
2021-03-28 16:20:53 +0300

    finished table logic

--- src/components/table/table.resize.js
+++ src/components/table/table.resize.js
@@ -13,37 +13,34 @@ export function resizeHandler($root, event) {
     [sideProp]: '-5000px'
   });
 
-  document.onmousemove = (e) => {
+  document.onmousemove = e => {
     if (type === 'col') {
       const delta = e.pageX - coords.right;
       value = coords.width + delta;
-      $resizer.css({
-        right: -delta + 'px',
-      });
+      $resizer.css({right: -delta + 'px'});
     } else {
       const delta = e.pageY - coords.bottom;
       value = coords.height + delta;
-      $resizer.css({
-        bottom: -delta + 'px'
-      });
+      $resizer.css({bottom: -delta + 'px'});
     }
   };
 
   document.onmouseup = () => {
     document.onmousemove = null;
     document.onmouseup = null;
+
+    if (type === 'col') {
+      $parent.css({width: value + 'px'});
+      $root.findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach(el => el.style.width = value + 'px');
+    } else {
+      $parent.css({height: value + 'px'});
+    }
+
     $resizer.css({
       opacity: 0,
       bottom: 0,
       right: 0
     });
-    $parent.css({width: value + 'px'});
-    if (type === 'col') {
-      $root
-          .findAll(`[data-col="${$parent.data.col}"]`)
-          .forEach(el => el.style.width = value + 'px');
-    } else {
-      $parent.css({height: value + 'px'});
-    }
   };
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -13,6 +13,17 @@ class Dom {
     return this.$el.outerHTML.trim();
   }
 
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text;
+      return this;
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim();
+    }
+    return this.$el.textContent.trim();
+  }
+
   clear() {
     this.html('');
     return this;
@@ -24,18 +35,23 @@ class Dom {
 
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback);
-    console.log(eventType);
+  }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector));
   }
 
   append(node) {
     if (node instanceof Dom) {
       node = node.$el;
     }
+
     if (Element.prototype.append) {
       this.$el.append(node);
     } else {
       this.$el.appendChild(node);
     }
+
     return this;
   }
 
@@ -51,14 +67,42 @@ class Dom {
     return this.$el.getBoundingClientRect();
   }
 
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
   css(styles = {}) {
     Object
         .keys(styles)
-        .forEach(key => this.$el.style[key] = styles[key]);
+        .forEach(key => {
+          this.$el.style[key] = styles[key];
+        });
   }
 
-  findAll(selector) {
-    return this.$el.querySelectorAll(selector);
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':');
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      };
+    }
+    return this.data.id;
+  }
+
+  focus() {
+    this.$el.focus();
+    return this;
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className);
+    return this;
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className);
+    return this;
   }
 }
 
@@ -66,8 +110,8 @@ export function $(selector) {
   return new Dom(selector);
 }
 
-$.create = (tagname, classes = '') => {
-  const el = document.createElement(tagname);
+$.create = (tagName, classes = '') => {
+  const el = document.createElement(tagName);
   if (classes) {
     el.classList.add(classes);
   }
