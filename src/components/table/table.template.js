2d81b33c301f0c672575979a0d629148ae71537c
Aliaksandr Snitko <pennywisexy@gmail.com>
2021-03-28 19:24:38 +0300

    added redux

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,14 +3,15 @@ const CODES = {
   Z: 90
 };
 
-// function toCell(row, col) {
-//   return `
-//     <div class="cell" contenteditable data-col="${col}"></div>
-//   `
-// }
+const DEFAULT_WIDTH = 120;
 
-function toCell(row) {
+function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px';
+}
+
+function toCell(state, row) {
   return function(_, col) {
+    const width = getWidth(state.colState, col);
     return `
       <div 
         class="cell" 
@@ -18,14 +19,20 @@ function toCell(row) {
         data-col="${col}"
         data-type="cell"
         data-id="${row}:${col}"
+        style="width: ${width}"
       ></div>
     `;
   };
 }
 
-function toColumn(col, index) {
+function toColumn({col, index, width}) {
   return `
-    <div class="column" data-type="resizable" data-col="${index}">
+    <div 
+      class="column" 
+      data-type="resizable" 
+      data-col="${index}"
+      style="width: ${width}"
+    >
       ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
@@ -37,7 +44,7 @@ function createRow(index, content) {
       ? '<div class="row-resize" data-resize="row"></div>'
       : '';
   return `
-    <div class="row" data-type="resizable">
+    <div class="row" data-type="resizable" data-row="${index}"></div>
       <div class="row-info">
         ${index ? index : ''}
         ${resize}
@@ -51,13 +58,22 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index);
 }
 
-export function createTable(rowsCount = 15) {
+function withWidthFrom(state) {
+  return function(col, index) {
+    return {
+      col, index, width: getWidth(state.colState, index)
+    };
+  };
+}
+
+export function createTable(rowsCount = 15, state = {}) {
   const colsCount = CODES.Z - CODES.A + 1; // Compute cols count
   const rows = [];
 
   const cols = new Array(colsCount)
       .fill('')
       .map(toChar)
+      .map(withWidthFrom(state))
       .map(toColumn)
       .join('');
 
@@ -66,8 +82,7 @@ export function createTable(rowsCount = 15) {
   for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        // .map((_, col) => toCell(row, col))
-        .map(toCell(row))
+        .map(toCell(state, row))
         .join('');
 
     rows.push(createRow(row + 1, cells));
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -6,39 +6,42 @@ export class ExcelComponent extends DomListener {
     this.name = options.name || '';
     this.emitter = options.emitter;
     this.unsubscribers = [];
+    this.store = options.store;
+    this.storeSub = null;
 
     this.prepare();
   }
 
-  // Настраивааем наш компонент до init
   prepare() {}
 
-  // Возвращает шаблон компонента
   toHTML() {
     return '';
   }
 
-  // Уведомляем слушателей про событие event
   $emit(event, ...args) {
     this.emitter.emit(event, ...args);
   }
 
-  // Подписываемся на событие event
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn);
     this.unsubscribers.push(unsub);
   }
 
-  // Инициализируем компонент
-  // Добавляем DOM слушателей
+  $dispatch(action) {
+    this.store.dispatch(action);
+  }
+
+  $subscribe(fn) {
+    this.storeSub = this.store.subscribe(fn);
+  }
+
   init() {
     this.initDOMListeners();
   }
 
-  // Удаляем компонент
-  // Чистим слушатели
   destroy() {
     this.removeDOMListeners();
     this.unsubscribers.forEach(unsub => unsub());
+    this.storeSub.unsubscribe();
   }
 }
